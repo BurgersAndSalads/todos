205beabe9018a737fac72e10955f037f0fcc5d0f
yiren <wang_yiren37@hotmail.com>
2020-12-10 01:25:31 -0500

    auth implemented, writes to db

--- src/Pages/App/App.js
+++ src/Pages/App/App.js
@@ -1,16 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { Switch, Redirect, Route } from 'react-router-dom';
+
+import SignupPage from '../SignupPage/SignupPage';
+import LoginPage from '../LoginPage/LoginPage';
+import userService from '../../utils/userService';
+import NavBar from '../../components/NavBar/NavBar';
 
 export default class App extends Component {
+
+
+
+  handleLogout = () => {
+    userService.logout();
+    this.setState({ user: null });
+  }
+
+  handleSignupOrLogin = () => {
+    this.setState({user: userService.getUser()});
+  }
+
+
   render() {
     return (
       <div>
-        <nav>
-          <Router>
-            <Link to="https://github.com/BurgersAndSalads/todos">home page</Link>
-          </Router>
-        </nav>
+        <NavBar />
+          <Switch>
+            <Route exact path='/signup' render={({ history }) => 
+              <SignupPage
+                history={history}
+                handleSignupOrLogin={this.handleSignupOrLogin}
+              />
+            }/>
+            <Route exact path='/login' render={({ history }) => 
+              <LoginPage
+                history={history}
+                handleSignupOrLogin={this.handleSignupOrLogin}
+              />
+            }/>
+            <Route exact path='/high-scores' render={() => 
+              userService.getUser() ? 
+                <h1>Logged In</h1>
+              :
+                <Redirect to='/login'/>
+            }/>
+          </Switch>
       </div>
     );
   } 
